c03ebae8c17ddbeab68740cd424e0bc1c9cb0de4
Santoshreddy-98 <gorantlasantosh1998@gmail.com>
2022-09-24 15:01:22 +0530

    ok

--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import "./Weather.css"
-import "./img2.jpg"
+// import "../Logo/img1.png"
 const Weather = () => {
     const [city,setCity] = useState("")
     const [result,setResult]=useState("")
@@ -28,11 +28,12 @@ const Weather = () => {
         <center>
         <div className="container mt-5 col-4">
             <div className="card">
-                <div className="card-header" style={{backgroundColor:"skyblue"}}>
-                <img src="./Logo/img1.png"/>
+                <div className="card-header">
+                <img src="https://media.istockphoto.com/photos/weather-forecast-concept-picture-id531889697?b=1&k=20&m=531889697&s=170667a&w=0&h=imZl8VcPhgj-6jTFFkYGbVTFOTyQ-u93Z9WsyhMdqps="
+                 height="150px" width="340px"  />
                     <h4 className="Card-title">Check Your City's Weather</h4>
                 </div>
-                <div className="card-body">
+                <div className="card-body"> 
                     <form onSubmit={onSubmitHandler}>
                         <input type="text" name="city" placeholder="Enter Your City Name" onChange={onChangeHandler} value={city}/><br/>
                         <input type="submit" className="btn btn-dark mt-2" /> <br/>
